c4bd7ef818d2b886f4175597dc2f461380331cf5
Kaio Lima Pimentel <dev.kaiolima.github@gmail.com>
2025-05-17 11:38:09 -0300

    Vlibras

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"; // Importa o Script do Next.js
 
 // Fontes
 const geistSans = Geist({
@@ -23,10 +24,11 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Nook",
-  description: "Plataforma de tecnologia da NOOK focada na inclusão de jovens no mercado de trabalho, com impacto social e desenvolvimento profissional transformador.",
+  description:
+    "Plataforma de tecnologia da NOOK focada na inclusão de jovens no mercado de trabalho, com impacto social e desenvolvimento profissional transformador.",
   icons: {
-    icon: '/favicon.ico',
-  }
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -36,11 +38,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-       <link rel="icon" href="/favicon.ico" />
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${poppins.variable} antialiased bg-white`}
       >
         {children}
+
+        {/* VLibras Plugin */}
+        <div vw className="enabled">
+          <div vw-access-button className="active"></div>
+          <div vw-plugin-wrapper>
+            <div className="vw-plugin-top-wrapper"></div>
+          </div>
+        </div>
+
+        <Script src="https://vlibras.gov.br/app/vlibras-plugin.js" strategy="afterInteractive" />
+        <Script
+          id="vlibras-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `new window.VLibras.Widget('https://vlibras.gov.br/app');`,
+          }}
+        />
       </body>
     </html>
   );
